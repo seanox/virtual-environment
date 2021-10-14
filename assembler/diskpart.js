d99fca299da5dc4d3557798a0c579e453c556bc0
seanox <service@seanox.de>
2021-10-14 20:45:18 +0200

    #0000 Assemble: Implementation

---
+++ assembler/diskpart.js
@@ -0,0 +1,46 @@
+/**
+ * LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt, im
+ * Folgenden Seanox Software Solutions oder kurz Seanox genannt.
+ * Diese Software unterliegt der Version 2 der Apache License.
+ *
+ * Portable Development Environment
+ * Copyright (C) 2021 Seanox Software Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Diskpart 1.0.0 20211014
+ * Copyright (C) 2021 Seanox Software Solutions
+ * All rights reserved.
+ *
+ * @author  Seanox Software Solutions
+ * @version 1.0.0 20211014
+ */
+import child from "child_process"
+import fs from "fs"
+
+import Workspace from "./workspace.js"
+
+export default class Diskpart {
+
+    static diskpartExec(diskpart) {
+        const diskpartWorkFile = Workspace.createWorkfile("./assembler/" + diskpart)
+        try {
+            const output = child.spawnSync("diskpart", ["/s", diskpartWorkFile])
+            if (output instanceof Error)
+                throw output
+            return output
+        } finally {
+            fs.unlinkSync(diskpartWorkFile)
+        }
+    }
+}
