fbf0b824f97fed0d174699284fba4f606a562ae2
seanox <service@seanox.de>
2021-11-05 11:13:29 +0100

    #0000 Platform: Implementation

--- platform/Resources/startup.cmd
+++ platform/Resources/startup.cmd
@@ -3,5 +3,5 @@ REM additional batch scripts are included in which the modules can add required
 REM batch calls and commands.
 REM [ ] Batch script with common commands
 REM [ ] Batch script for initialization, will be executed after common
-REM [ ] Batch script after initialization for program starts
-REM [X] Batch script when exiting the environment
+REM [X] Batch script after initialization for program starts
+REM [ ] Batch script when exiting the environment
