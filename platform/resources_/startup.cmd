fdb04dede4487b8ddd6e10014b5ec4eb3e3e2299
seanox <service@seanox.de>
2021-11-06 20:43:33 +0100

    #0000 Documentation: Update of content

---
+++ platform/resources_/startup.cmd
@@ -0,0 +1,7 @@
+REM During the startup and detachment of the virtual environment, other
+REM additional batch scripts are included in which the modules can add required
+REM batch calls and commands.
+REM [ ] Batch script with common commands
+REM [ ] Batch script for initialization, will be executed after common
+REM [X] Batch script after initialization for program starts
+REM [ ] Batch script when exiting the environment
