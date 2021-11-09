156e20591fc0710a2666d86cbd53c1775fa856ca
seanox <service@seanox.de>
2021-11-09 08:41:50 +0100

    #0000 Platform: Implementation

--- platform/resources/detach.cmd
+++ platform/resources/detach.cmd
@@ -1,7 +1,7 @@
 REM During the startup and detachment of the virtual environment, other
 REM additional batch scripts are included in which the modules can add required
 REM batch calls and commands.
-REM [ ] Batch script with common commands
-REM [ ] Batch script for initialization, will be executed after common
-REM [ ] Batch script after initialization for program starts
-REM [X] Batch script when exiting the environment
+REM - Batch script with common commands
+REM - Batch script for initialization, will be executed after common
+REM - Batch script after initialization for program starts
+REM + Batch script when exiting the environment
