b477f28f5ed48893c71327e848c1b3c69e8b4d6c
seanox <service@seanox.de>
2021-11-27 09:38:12 +0100

    #0000 Platform: Preparation of next version

--- platform/Properties/AssemblyInfo.cs
+++ platform/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("accc1bbb-2740-490d-beba-cbf027a57b4b")]
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
