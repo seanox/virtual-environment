fee1e6d5f48bcf144f32881fe417fbcba503be7a
seanox <service@seanox.de>
2022-01-06 18:09:56 +0100

    #0000 Shiftdown: Correction of AssemblyInfo.cs for the build processs

--- shiftdown/Properties/AssemblyInfo.cs
+++ shiftdown/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Seanox Software Solutions")]
 [assembly: AssemblyProduct("Shiftdown")]
-[assembly: AssemblyCopyright("Copyright © 2022 Seanox Software Solutions")]
+[assembly: AssemblyCopyright("Copyright © 0000 Seanox Software Solutions")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
