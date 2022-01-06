89ec8d20e635a4a14e59f8e6932ad4e21a166954
seanox <service@seanox.de>
2022-01-06 19:09:57 +0100

    #0000 Launcher: Implementation

---
+++ launcher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Launcher;
+
+namespace launcher
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Worker());
+        }
+    }
+}
