6ebd59324570e8dfc59af6b88f373aa88bf18bfd
seanox <service@seanox.de>
2022-01-07 18:45:38 +0100

    #0000 Launcher: Implementation

--- launcher/Program.cs
+++ launcher/Program.cs
@@ -1,20 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt, im
+// Folgenden Seanox Software Solutions oder kurz Seanox genannt.
+// Diese Software unterliegt der Version 2 der Apache License.
+//
+// Virtual Environment Launcher
+// Program starter for the virtual environment.
+// Copyright (C) 2022 Seanox Software Solutions
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using System;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 using Launcher;
 
 namespace launcher
 {
-    static class Program
+    internal static class Program
     {
+        private static Settings _settings;
+        
         [STAThread]
-        static void Main()
+        internal static void Main()
         {
+            var applicationPath = Assembly.GetExecutingAssembly().Location;
+            var applicationConfigurationFile = Path.Combine(Path.GetDirectoryName(applicationPath),
+                    Path.GetFileNameWithoutExtension(applicationPath) + ".xml");
+
+            var serializer = new XmlSerializer(typeof(Settings));
+            using (var reader = new StreamReader(applicationConfigurationFile))
+                _settings = (Settings)serializer.Deserialize(reader);
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Worker());
+            Application.Run(new Worker(_settings));
         }
     }
 }
