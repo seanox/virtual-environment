ad7d63f58390d1bb983181e3eca87005b1bfd373
seanox <service@seanox.de>
2022-02-12 11:31:41 +0100

    #0000 Launcher: Implementation

---
+++ launcher/Tiles/MetaTileGrid.cs
@@ -0,0 +1,55 @@
+﻿// LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt, im
+// Folgenden Seanox Software Solutions oder kurz Seanox genannt.
+// Diese Software unterliegt der Version 2 der Apache License.
+//
+// Virtual Environment Launcher
+// Program starter for the virtual environment.
+// Copyright (C) 2022 Seanox Software Solutions
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using System.Windows.Forms;
+
+namespace Seanox.Platform.Launcher.Tiles
+{
+    internal class MetaTileGrid
+    {
+        internal readonly int Size;
+        internal readonly int Gap;
+        internal readonly int Columns;
+        internal readonly int Rows;
+        internal readonly int Padding;
+
+        internal int Count  => Columns * Rows;
+        internal int Height => ((Size + Gap) * Rows) - Gap;
+        internal int Width  => ((Size + Gap) * Columns) - Gap;
+        
+        private readonly Settings Settings;
+
+        private MetaTileGrid(Settings settings, int columns, int rows)
+        {
+            Settings = settings;
+            Rows     = rows;
+            Columns  = columns;
+            
+            Size    = Settings.GridSize;
+            Gap     = Settings.GridGap;
+            Padding = Settings.GridPadding;
+        }
+
+        internal static MetaTileGrid Create(Settings settings, int columns, int rows)
+        {
+            return new MetaTileGrid(settings, columns, rows);
+        }
+    }
+}
