15e48d48ccb73181a321eea413e4d05dc30e8380
seanox <service@seanox.de>
2022-02-15 20:29:39 +0100

    #0000 Launcher: Implementation

--- launcher/Control.Designer.cs
+++ launcher/Control.Designer.cs
@@ -31,39 +31,22 @@ namespace Seanox.Platform.Launcher
         /// </summary>
         private void InitializeComponent()
         {
-            this.Message = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // Message
-            // 
-            this.Message.BackColor = System.Drawing.Color.Transparent;
-            this.Message.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Message.ForeColor = System.Drawing.Color.White;
-            this.Message.Location = new System.Drawing.Point(0, 0);
-            this.Message.Name = "Message";
-            this.Message.Size = new System.Drawing.Size(100, 100);
-            this.Message.TabIndex = 0;
-            this.Message.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Control
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(800, 600);
             this.ControlBox = false;
-            this.Controls.Add(this.Message);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Control";
-            this.Opacity = 0.75D;
+            this.Opacity = 0.9D;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.TopMost = true;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
         }
-        
-        private System.Windows.Forms.Label Message;
 
         #endregion
     }
