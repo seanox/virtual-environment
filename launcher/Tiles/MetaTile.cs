57e502b579ce0ff0f401868347028638e05a4aa5
seanox <service@seanox.de>
2022-02-21 07:28:03 +0100

    #0000 Launcher: Implementation

--- launcher/Tiles/MetaTile.cs
+++ launcher/Tiles/MetaTile.cs
@@ -99,7 +99,7 @@ namespace Seanox.Platform.Launcher.Tiles
 
         private static int CalculateIconSize(int iconSpace)
         {
-            var iconSizes = new int[] {16, 24, 32, 48, 64, 128, 256, 512};
+            var iconSizes = new [] {16, 24, 32, 48, 64, 128, 256, 512};
             Array.Reverse(iconSizes);
             foreach (var iconSize in iconSizes)
                 if (iconSpace >= iconSize)
