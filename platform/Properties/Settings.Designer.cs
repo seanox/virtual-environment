68d42bff616af273a896b342ece6af29bafc3df1
seanox <service@seanox.de>
2022-02-27 21:19:25 +0100

    #0000 Platform: Unification of the namespace

--- platform/Properties/Settings.Designer.cs
+++ platform/Properties/Settings.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Seanox.Platform.Properties {
+﻿namespace VirtualEnvironment.Platform.Properties {
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
