573d4123766af847a258522148a3bdb6428d8c2c
seanox <service@seanox.de>
2022-02-27 19:15:50 +0100

    #0000 StartUp: Optimization and corrections

--- startup/Program.cs
+++ startup/Program.cs
@@ -4,7 +4,7 @@
 //
 // Virtual Environment Startup
 // Starts a batch script with the same name minimized.
-// Copyright (C) 2021 Seanox Software Solutions
+// Copyright (C) 2022 Seanox Software Solutions
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy of
