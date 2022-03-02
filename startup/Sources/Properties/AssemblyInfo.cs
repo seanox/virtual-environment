6a550ef1cdccc060c86ad9ca90b0d9de774af683
seanox <service@seanox.de>
2022-03-02 10:36:37 +0100

    #0000 StartUp: Unification of (sub) project structure

---
+++ startup/Sources/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Starts batch scripts in a minimized console window.")]
+[assembly: AssemblyDescription("Starts batch scripts in a minimized console window.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Seanox Software Solutions")]
+[assembly: AssemblyProduct("Startup")]
+[assembly: AssemblyCopyright("Copyright © 0000 Seanox Software Solutions")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components. If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("131df921-4d19-4cdb-a6e0-784447653e7b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
