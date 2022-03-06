07f12e86b9dbda5702e8e9c73179e3a855932071
seanox <service@seanox.de>
2022-03-06 21:19:44 +0100

    #0000 ShiftDown: Optimization and corrections

--- shiftdown/Sources/Program.cs
+++ shiftdown/Sources/Program.cs
@@ -19,16 +19,12 @@
 // the License.
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Security.Principal;
 using System.ServiceProcess;
 using System.Text.RegularExpressions;
-using System.Threading;
 
 namespace VirtualEnvironment.ShiftDown
 {
@@ -65,7 +61,7 @@ namespace VirtualEnvironment.ShiftDown
                 Console.WriteLine(VERSION);
                 Console.WriteLine();
 
-                bool isAdministrator;
+                var isAdministrator = false;
                 using (var identity = WindowsIdentity.GetCurrent())
                     isAdministrator = new WindowsPrincipal(identity).IsInRole(WindowsBuiltInRole.Administrator);
                 
@@ -139,329 +135,35 @@ namespace VirtualEnvironment.ShiftDown
                 Console.WriteLine();
             }
 
-            Process process = new Process();
-            process.StartInfo = new ProcessStartInfo()
+            using (var process = new Process())
             {
-                UseShellExecute = false,
-                CreateNoWindow  = true,
-
-                WindowStyle = ProcessWindowStyle.Hidden,
-
-                FileName  = batchExecMeta.FileName,
-                Arguments = String.Join(" ", batchExecMeta.Arguments),
-
-                RedirectStandardError  = true,
-                RedirectStandardOutput = true
-            };
-            process.Start();
-            process.WaitForExit();
-
-            if (!batchExecMeta.Output)
-                return;
-            
-            var standardErrorOutput = (process.StandardError.ReadToEnd()).Trim();
-            if (standardErrorOutput.Length > 0)
-                Console.Error.Write(standardErrorOutput + Environment.NewLine);
-            
-            var standardOutput = (process.StandardOutput.ReadToEnd()).Trim();
-            if (standardOutput.Length > 0)
-                Console.Out.Write(standardOutput + Environment.NewLine);
-        }
-    }
-    
-    internal class Service : ServiceBase
-    {
-        private const int MAXIMUM_CPU_LOAD_PERCENT = 25;
-        private const int MEASURING_TIME_MILLISECONDS = 1000;
-
-        private BackgroundWorker _backgroundWorker;
-        
-        private bool _interrupt;
-        
-        private struct ProcessMonitor
-        {
-            internal Process Process;
-            internal ProcessPriorityClass PriorityClassInitial;
-            internal PerformanceCounter PerformanceCounter;
-        }
-
-        private readonly List<ProcessMonitor> _processMonitorsDecreased;  
-        
-        private readonly Dictionary<int, ProcessMonitor> _processMonitors;
-
-        private readonly EventLog _eventLog;
-
-        internal Service()
-        {
-            _eventLog = new EventLog();
-            _eventLog.Source = Program.ApplicationMeta.Name; 
-            
-            CanStop = true;
-            CanPauseAndContinue = true;
-            AutoLog = false;
-
-            _processMonitors = new Dictionary<int, ProcessMonitor>();
-            _processMonitorsDecreased = new List<ProcessMonitor>();
-
-            // The own priority must be higher than normal, so that the service
-            // itself gets enough CPU time at high load.
-
-            // The service process is treated as a system process and uses an
-            // empty ProcessMonitor, so it is excluded from prioritization.
-
-            var process = Process.GetCurrentProcess();
-            process.PriorityClass = ProcessPriorityClass.AboveNormal;
-            _processMonitors.Add(process.Id, new ProcessMonitor());
-        }
-
-        private void DecreasePrioritySmart(List<ProcessMonitor> processMonitors)
-        {
-            // Processes where access to the priority is not allowed are
-            // filtered and ignored (e.g. system processes)
-            processMonitors = processMonitors.FindAll(processMonitor => processMonitor.Process != null);
-            
-            foreach (var processMonitor in processMonitors)
-            {
-                Thread.Sleep(25);
-                
-                try
+                process.StartInfo = new ProcessStartInfo()
                 {
-                    var process = processMonitor.Process;
-                    
-                    // Discards all information about the assigned process that
-                    // was cached in the process component. Microsoft also
-                    // likes to cache and it took me a while to understand why
-                    // the priority is not always up to date :-)
-                    process.Refresh();
-                    if (ProcessPriorityClass.Idle.Equals(process.PriorityClass))
-                        continue;
+                    UseShellExecute = false,
+                    CreateNoWindow  = true,
 
-                    var cpuLoad = processMonitor.PerformanceCounter.NextValue() / Environment.ProcessorCount;
-                    if (cpuLoad < MAXIMUM_CPU_LOAD_PERCENT)
-                        continue;
+                    WindowStyle = ProcessWindowStyle.Hidden,
 
-                    process.PriorityClass = ProcessPriorityClass.Idle;
-                    
-                    if (!_processMonitorsDecreased.Contains(processMonitor))
-                        _processMonitorsDecreased.Add(processMonitor);
-                }
-                catch (InvalidOperationException)
-                {
-                    _processMonitors.Remove(processMonitor.Process.Id);
-                }
-                catch (Exception)
-                {
-                }
+                    FileName  = batchExecMeta.FileName,
+                    Arguments = String.Join(" ", batchExecMeta.Arguments),
+
+                    RedirectStandardError  = true,
+                    RedirectStandardOutput = true
+                };
+                process.Start();
+                process.WaitForExit();
+
+                if (!batchExecMeta.Output)
+                    return;
+
+                var standardErrorOutput = (process.StandardError.ReadToEnd()).Trim();
+                if (standardErrorOutput.Length > 0)
+                    Console.Error.Write(standardErrorOutput + Environment.NewLine);
+            
+                var standardOutput = (process.StandardOutput.ReadToEnd()).Trim();
+                if (standardOutput.Length > 0)
+                    Console.Out.Write(standardOutput + Environment.NewLine);
             }
-        }
-
-        private void RestorePriority()
-        {
-            foreach (var processMonitor in _processMonitors.Values)
-            {
-                try
-                {
-                    processMonitor.Process.PriorityClass = processMonitor.PriorityClassInitial;
-                }
-                catch (Exception)
-                {
-                }
-            }
-        }
-
-        private BackgroundWorker CreateBackgroundWorker()
-        {
-            var backgroundWorker = new BackgroundWorker();
-            backgroundWorker.WorkerSupportsCancellation = true;
-            backgroundWorker.WorkerReportsProgress = false;
-            backgroundWorker.DoWork += (sender, eventArguments) =>
-            {
-                // How it works:
-                // - Periodic measurement of the total CPU load
-                //   From a load  of 25 percent (percentage on all cores) the
-                //   priorities of the processes start to be checked
-                // - Scan all processes and determine the new processes for
-                //   which no process monitor with PerformanceCounter exists
-                //   and create that for the process.
-                // - Analyze CPU consumption based on process monitors and try
-                //   to optimize. In doing so, clean up all process monitors
-                //   for processes that no longer exist. 
-                
-                // Analysis:
-                // - Check if the process still exists, if not clean it up
-                // - If the priority is less than NORMAL, then ignore the process
-                // - Measure the load of the "main process" (process name
-                //   -- unfortunately I didn't find another easy way)
-                // - If the load of the main process is greater than or equal
-                //   to 25 percent, try to reduce the priority of the process
-                
-                // Attempted optimization of processes:
-                // - In case of high load, the priority of the processes with
-                //   high load is temporarily set to Idle
-                // - If the general CPU load decreases to a quarter of the
-                //   threshold value, the processes are set to BelowNormal
-                // - Original priority normal or higher is restored only at the
-                //   end of the service.
-                
-                var cpuLoadTiming = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-                
-                var cpuUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-                cpuUsage.NextValue();
-
-                while (!_backgroundWorker.CancellationPending)
-                {
-                    Thread.Sleep(MEASURING_TIME_MILLISECONDS);
-                    if (_interrupt)
-                        continue;
-                    
-                    // There are different views on the interpretation of the
-                    // value. I refer to the statement that the value
-                    // represents the average over all cores. Over 100% is also
-                    // possible -- calculative or when the CPU uses Turbo Boost.
-                    // Per WMI each core can be queried, but it probably
-                    // doesn't matter with the question if the CPU is generally
-                    // loaded.
-                    
-                    // The list of the ProcessMonitor always contains at least
-                    // the process of the service. The use of Any function is
-                    // not possible here.
-                    
-                    var cpuUsageCurrent = cpuUsage.NextValue();
-                    if (cpuUsageCurrent < MAXIMUM_CPU_LOAD_PERCENT
-                            && _processMonitors.Count() > 1)
-                    {
-                        // Increasing the priority is done with a delay, so
-                        // that the priority is not switched up and down
-                        // excessively -- because it can have effects on the
-                        // process. 5 seconds is based on the assumption that
-                        // a program will try to do things quickly and avoid
-                        // delays of several seconds.  
-                        
-                        if (cpuUsageCurrent < MAXIMUM_CPU_LOAD_PERCENT / 4
-                                && _processMonitorsDecreased.Any()
-                                && DateTimeOffset.Now.ToUnixTimeMilliseconds() - cpuLoadTiming < 5000)
-                        {
-                            _processMonitorsDecreased.ForEach(processMonitor =>
-                            {
-                                try
-                                {
-                                    processMonitor.Process.PriorityClass = ProcessPriorityClass.BelowNormal;
-                                }
-                                catch (Exception)
-                                {
-                                }
-                            });
-                            _processMonitorsDecreased.Clear();
-                        } else cpuLoadTiming = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-                        continue;
-                    }
-                    
-                    // Control of the known loaders.
-                    DecreasePrioritySmart(_processMonitorsDecreased);
-
-                    // To determine only new processes Except is a nice
-                    // function for deltas in lists used for this purpose.
-                    var processList = Process.GetProcesses().ToList();
-                    var processesListCurrent = (from process in processList select process.Id).ToList();
-                    var processListDelta = processesListCurrent.Except(_processMonitors.Keys.ToList());
-                    processList = processList.FindAll(process => processListDelta.Contains(process.Id));
-
-                    foreach (var process in processList)
-                    {
-                        if (_processMonitors.ContainsKey(process.Id))
-                            continue;
-                        
-                        // Access to the priority of system processes is not
-                        // allowed. For these processes an empty ProcessMonitor
-                        // is created so that these processes are not detected
-                        // as new each time.
-
-                        var processMonitor = new ProcessMonitor();
-                        _processMonitors.Add(process.Id, processMonitor);
-                        
-                        try
-                        {
-                            processMonitor.Process = process;
-                            processMonitor.PriorityClassInitial = process.PriorityClass;
-                            processMonitor.PerformanceCounter = new PerformanceCounter("Process", "% Processor Time",
-                                    process.ProcessName, true);
-                            processMonitor.PerformanceCounter.NextValue();
-                            
-                            // Structures behave are strange :-|
-                            // Each change of the values creates a new
-                            // instance. That's why the existing reference in
-                            // the list is an original of which with each
-                            // change a copy is created as a new instance.
-                            // Therefore the entry in the list does not get
-                            // anything from the changes and so the changed
-                            // instance must be packed into the list. 
-                            _processMonitors[process.Id] = processMonitor;
-                        }
-                        catch (Exception)
-                        {
-                        }
-                    }
-
-                    DecreasePrioritySmart(_processMonitors.Values.ToList());
-                    
-                    cpuLoadTiming = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-                }
-
-                RestorePriority();
-            };
-
-            return backgroundWorker;
-        }
-
-        internal void OnDebug(params string[] options)
-        {
-            OnStart(options);
-            while (!_backgroundWorker.CancellationPending
-                    && !_interrupt)
-                Thread.Sleep(1000);
-            OnStop();
-        }
-
-        protected override void OnStart(string[] options)
-        { 
-            if (_backgroundWorker != null
-                    && _backgroundWorker.IsBusy)
-                return;
-
-            _eventLog.WriteEntry(Program.VERSION, EventLogEntryType.Information);
-
-            _backgroundWorker = CreateBackgroundWorker();
-            _backgroundWorker.RunWorkerAsync();
-
-            _eventLog.WriteEntry("Service started.", EventLogEntryType.Information);
-        }
-
-        protected override void OnPause()
-        {
-            if (_interrupt)
-                return;
-            _interrupt = true;
-            _eventLog.WriteEntry("Service paused.", EventLogEntryType.Information);
-        }
-
-        protected override void OnContinue()
-        {
-            if (!_interrupt)
-                return;
-            _interrupt = false;
-            _eventLog.WriteEntry("Service continued.", EventLogEntryType.Information);
-        }
-
-        protected override void OnStop()
-        {
-            if (!_backgroundWorker.IsBusy)
-                return;
-
-            _backgroundWorker.CancelAsync();
-            while (_backgroundWorker.IsBusy)
-                Thread.Sleep(25);
-            _backgroundWorker = null;
-            _eventLog.WriteEntry("Service stopped.", EventLogEntryType.Information);
         }
     }
 }
