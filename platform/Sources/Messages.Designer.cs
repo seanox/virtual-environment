997fc4430e20901ce1cb7a0c0105dad213baf483
seanox <service@seanox.de>
2022-03-10 07:52:39 +0100

    #0000 Platform: Unification of (sub) project structure

--- platform/Sources/Messages.Designer.cs
+++ platform/Sources/Messages.Designer.cs
@@ -39,7 +39,7 @@ namespace VirtualEnvironment.Platform {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VirtualEnvironment.Platform.Messages", typeof(Messages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VirtualEnvironment.Platform.Sources.Messages", typeof(Messages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
