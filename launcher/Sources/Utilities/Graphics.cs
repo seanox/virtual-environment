8b7e07ce6fac1a3234889bbf07663cf7e22ecc51
seanox <service@seanox.de>
2022-05-17 16:28:21 +0200

    #0000 Review: Optimization and corrections

--- launcher/Sources/Utilities/Graphics.cs
+++ launcher/Sources/Utilities/Graphics.cs
@@ -91,7 +91,7 @@ namespace VirtualEnvironment.Launcher.Utilities
                 using (var wrapMode = new ImageAttributes())
                 {
                     wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width,image.Height, GraphicsUnit.Pixel, wrapMode);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
                 }
             }
 
