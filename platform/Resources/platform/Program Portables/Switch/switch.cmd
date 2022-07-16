e354e016d7eb4158ad3b288954ef80a1f2953b92
seanox <service@seanox.de>
2022-07-16 10:08:11 +0200

    #0000 Switch: Temporary change of the command line configuration

--- platform/Resources/platform/Program Portables/Switch/switch.cmd
+++ platform/Resources/platform/Program Portables/Switch/switch.cmd
@@ -5,7 +5,7 @@ ECHO Copyright (C) 0000 Seanox Software Solutions
 ECHO Temporary change of the command line configuration
 ECHO.
 
-SETLOCAL
+SETLOCAL ENABLEEXTENSIONS
 SET WORKDIR=%~dp0
 SET WORKPATCH=%1
 SET WORKPATCH=%WORKPATCH:"=%.cmd
@@ -21,8 +21,21 @@ IF NOT [%1] == [] (
 ECHO Usage: %~nx0 [patch]
 ECHO.
 
-REM TODO: list of available patches (names only)
-REM       otherwise: no patches available
+SET PATCHESEXISTS=0
+FOR %%f IN (.\*.cmd) DO (
+    IF NOT [%%~nxf] == [%~nx0] (
+        SET PATCHESEXISTS=1
+    )
+)
+IF [%PATCHESEXISTS%] == [1] (
+    ECHO Available Patches:
+    FOR %%f IN (.\*.cmd) DO (
+        IF NOT [%%~nxf] == [%~nx0] (
+            ECHO   %%~nf
+        )
+    )
+) ELSE (
+    ECHO no patches available
+)
 
 ENDLOCAL
-GOTO :EOF
