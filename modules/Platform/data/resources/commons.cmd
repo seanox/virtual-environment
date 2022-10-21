46869a1b60e3984df81a832495c8091cc6a45989
seanox <service@seanox.de>
2022-10-21 07:14:54 +0200

    #0000 Modules: Removed until implementation starts

--- modules/Platform/data/resources/commons.cmd
+++
@@ -1,7 +0,0 @@
-REM During the startup and detachment of the virtual environment, other
-REM additional batch scripts are included in which the modules can add required
-REM batch calls and commands.
-REM + Batch script with common commands
-REM - Batch script for initialization, will be executed after common
-REM - Batch script after initialization for program starts
-REM - Batch script when exiting the environment
