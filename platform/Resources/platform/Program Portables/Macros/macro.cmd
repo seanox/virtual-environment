79a2e044bfb095495c802afb0bf2b99cefd35ada
seanox <service@seanox.de>
2023-01-25 20:35:06 +0100

    #0000 Macros: Replacement for Switch

---
+++ platform/Resources/platform/Program Portables/Macros/macro.cmd
@@ -0,0 +1,45 @@
+@ECHO OFF
+
+SET WORKDIR=%~dp0
+SET MACRO=%1
+SET MACRO=%MACRO:"=%.cmd
+SET MACROSDIR=%WORKDIR%\macros
+
+IF NOT EXIST "%MACROSDIR%" MKDIR "%MACROSDIR%"
+
+IF NOT [%1] == [] (
+    IF EXIST "%MACROSDIR%\%MACRO%" (
+         PUSHD "%MACROSDIR%" & "%MACRO%" & POPD
+         ENDLOCAL   
+         GOTO:EOF
+    )
+)
+
+SETLOCAL ENABLEEXTENSIONS
+
+ECHO Virtual Environment Macros [Version 1.0.0 20230125]
+ECHO Copyright (C) 2023 Seanox Software Solutions
+ECHO Temporary change of the command line configuration
+ECHO.
+
+ECHO usage: %~nx0 [macro]
+ECHO.
+
+SET MACROSEXISTS=0
+FOR %%f IN ("%MACROSDIR%\*.cmd") DO (
+    IF NOT [%%~nxf] == [%~nx0] (
+        SET MACROSEXISTS=1
+    )
+)
+IF [%MACROSEXISTS%] == [1] (
+    ECHO available macros:
+    FOR %%f IN ("%MACROSDIR%\*.cmd") DO (
+        IF NOT [%%~nxf] == [%~nx0] (
+            ECHO   %%~nf
+        )
+    )
+) ELSE (
+    ECHO no macros available
+)
+
+ENDLOCAL
