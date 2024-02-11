3b58ea45e3c32daddb0c1f00ed3714d74728239e
seanox <service@seanox.de>
2024-02-11 20:16:23 +0100

    #0000 Platform: Optimization and corrections

--- platform/Resources/platform/Program Portables/Launcher/launcherExit.cmd
+++
@@ -1,6 +0,0 @@
-REM Workaround and part of the concept. In the launcher an exit function can be
-REM mapped, but the virtual environment is kept alive by the launcher and
-REM therefore it does not exist as a default and so the launcher should be
-REM terminated via taskkill on detach.
-taskkill /t /im launcher.exe
-exit
