13fa2647002493576f440528e0b2d5509b147ece
seanox <service@seanox.com>
2025-03-23 11:04:47 +0100

    #0000 Review: Optimization and corrections

--- launcher/Sources/Utilities/ScanCode.cs
+++ launcher/Sources/Utilities/ScanCode.cs
@@ -4,7 +4,7 @@
 //
 // Virtual Environment Launcher
 // Program starter for the virtual environment.
-// Copyright (C) 2022 Seanox Software Solutions
+// Copyright (C) 2025 Seanox Software Solutions
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy of
