dad533055c0ae2965696bb1f1515db0dfcff23d1
seanox <service@seanox.com>
2025-04-19 14:22:51 +0200

    #0000 StartUp: Added support for portable applications

--- startup/Sources/Runner.cs
+++ startup/Sources/Runner.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿// LICENSE TERMS - Seanox Software Solutions is an open source project,
+// hereinafter referred to as Seanox Software Solutions or Seanox for short.
+// This software is subject to version 2 of the Apache License.
+//
+// Virtual Environment Startup
+// Program starter for the virtual environment.
+// Copyright (C) 2025 Seanox Software Solutions
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
