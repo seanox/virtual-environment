82e423fc1600b4f816a971eb4596cdb8dafa0efe
seanox <service@seanox.com>
2025-04-20 08:46:09 +0200

    #0000 Review: Optimization and corrections

---
+++ platform/Sources/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace VirtualEnvironment.Platform
+{
+    internal partial class Resources
+    {
+        internal static ResourceFiles Files { get; } = new ResourceFiles();
+        internal static ResourceTexts Texts { get; } = new ResourceTexts();
+        
+        private static byte[] GetResource(string resourceName)
+        {
+            resourceName = $"{typeof(Resources).Namespace}.Resources.{resourceName}";
+            resourceName = new Regex("[\\./\\\\]+").Replace(resourceName, ".");
+            resourceName = new Regex("\\s").Replace(resourceName, "_");
+            var assembly = Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+                using (var memoryStream = new MemoryStream())
+                {
+                    stream.CopyTo(memoryStream);
+                    return memoryStream.ToArray();
+                }
+        }
+
+        internal class ResourceFiles
+        {
+            internal byte[] this[string resourceName] =>
+                GetResource(resourceName);
+        }
+
+        internal class ResourceTexts
+        {
+            internal string this[string resourceName] =>
+                Encoding.UTF8.GetString(GetResource(resourceName));
+        }
+    }
+}
