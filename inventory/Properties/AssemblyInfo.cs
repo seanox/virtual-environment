05126e488cf3efcd7f90f53022bc14d7f30ae263
seanox <service@seanox.com>
2025-05-10 07:12:05 +0200

    #0000 Inventory: Implementation

---
+++ inventory/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Scans and extracts changes in the file system and registry.")]
+[assembly: AssemblyDescription("Scans and extracts changes in the file system and registry.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Seanox Software Solutions")]
+[assembly: AssemblyProduct("Inventory")]
+[assembly: AssemblyCopyright("Copyright © 2025 Seanox Software Solutions")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components. If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e23f6a19-4c7d-482b-9f90-3d65c1a8a177")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyMetadata("Build", "00000000")]
